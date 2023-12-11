4187358ceb263e3c326df563c177e72e6b37fc81
faizankhan33374 <153091344+faizankhan33374@users.noreply.github.com>
2023-12-11 11:19:48 +0530

    Add files via upload

---
+++ ChatGPT PRACTISE SET.py
@@ -0,0 +1,54 @@
+# Declare variables
+name = "John"
+age = 25
+height = 1.75
+
+# Print variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+# Practise
+
+name = "JOHN"
+age = "25"
+height = "1.75"
+
+print(name)
+print(age)
+print(height)
+
+name = "faizan ahmad gouri"
+age = "24"
+height = "5.8"
+print(name)
+print(age)
+print(height)
+
+name = "bobs"
+age = "24"
+height = "6.2"
+
+print("name:", name)
+print("age:", age)
+print("height", height)
+
+name = "bhaiya kabadi"
+age = "58"
+height = "5.6"
+print("name:", name)
+print("age:", age)
+print("height:", height)
+
+# Get user input
+
+# name = input("Entre your name :")
+# print("or sunao,"+ name + "!")
+
+# Conditions and if statements
+# Simple " if " statement
+x = 10
+# if x > 5:
+#  print("x is greater than 5")
+x = 15
+if x < 6:
+    print("x is greater than 15")
